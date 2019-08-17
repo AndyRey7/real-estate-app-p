c05596ba6394f340363de343d4443d10904f94a4
github username <github email address>
2019-08-17 10:47:08 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>
           whatever it works!!!!!!!!!!!
         </p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React I did
         </a>
       </header>
     </div>
