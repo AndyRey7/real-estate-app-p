f66b8a5abf4ed01e60ace1fd46a48e8e107b2270
github username <github email address>
2019-08-17 23:55:26 -0400

    everything is working great, started on the header for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header'
+import './scss/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
+    <div>
+        <Header/>
+        <p className="test">
           whatever it works!!!!!!!!!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React I did
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header>
+                <div>Logo</div>
+                <nav></nav>
+            </header>
+    )
+}
+export default Header
